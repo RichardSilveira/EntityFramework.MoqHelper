64371bbb011264c6b04305a1a90befcee0ffe4a3
RichardSilveira <richardleecba@gmail.com>
2015-10-31 10:35:37 -0300

    Working on test projetct

--- DemoClientUnitTest/CustomerSpecs.cs
+++ DemoClientUnitTest/CustomerSpecs.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using EntityFramework.MoqHelper;
 using DemoClientApp.DAL;
 using Moq;
+using System.Linq;
+using FluentAssertions;
 
 namespace DemoClientUnitTest
 {
@@ -52,6 +54,33 @@ namespace DemoClientUnitTest
             Assert.IsTrue(customer.CustomerID > 0);
         }
 
-        //todo:Create another method with Remove and Find operations
+        [TestMethod]
+        public void Customer_CRUD_operations_basics()
+        {
+            var customer = new Customer() { Name = "Foo Bar", Address = "Los Angeles, CA" };
+
+            var customers = new List<Customer>();
+            var mockSet = EntityFrameworkMoqHelper.CreateMockForDbSet<Customer>()
+                                                            .SetupForQueryOn(customers)
+                                                            .WithAdd(customers, "CustomerID")//overwritten to simulate behavior of auto-increment database
+                                                            .WithFind(customers, "CustomerID")
+                                                            .WithRemove(customers);
+
+            var mockContext = EntityFrameworkMoqHelper.CreateMockForDbContext<DemoContext, Customer>(mockSet);
+
+            var customerService = new CustomerService(mockContext.Object);
+
+            customerService.Insert(customer);
+
+            customers.Should().Contain(x => x.CustomerID == customer.CustomerID);
+
+            //Testing GetByID (and DbSet.Find) method
+            customerService.GetByID(customer.CustomerID).Should().NotBeNull();
+
+            //Testing Remove method
+            customerService.Remove(customer);
+
+            customerService.GetByID(customer.CustomerID).Should().BeNull();
+        }
     }
 }
